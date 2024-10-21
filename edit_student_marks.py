8c2211355dbb4c8063f44a08f5c157eed9facd6e
dsgeorge3005 <dsgeorge3005@gmail.com>
2024-10-21 15:00:45 +0800

    change

--- edit_student_marks.py
+++ edit_student_marks.py
@@ -4,13 +4,8 @@ from tkinter import ttk, messagebox
 import mysql.connector
 
 # DATABASE CONNECTION SETUP
-db = mysql.connector.connect(
-    host='mysql-336e5914-anirudhpranesh-be68.f.aivencloud.com',
-    port=13426,
-    user='avnadmin',
-    password='AVNS_1UgkIMxSzsCWt0D-3cB',
-    database='scholarmate_db'
-)
+db=mysql.connector.connect(host='localhost', user='root', password='Admin@1122', database='scholarmate_db') #local host conn.
+#db = mysql.connector.connect(host='mysql-336e5914-anirudhpranesh-be68.f.aivencloud.com',port=13426,user='avnadmin',password='AVNS_1UgkIMxSzsCWt0D-3cB',database='scholarmate_db')
 
 # Gracefully close the application and the database connection
 def on_close():
